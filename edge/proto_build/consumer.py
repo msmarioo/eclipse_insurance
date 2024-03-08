156e646805b964d3b2a900322e8e6f468b37d668
msmarioo <marioo@microsoft.com>
2024-03-08 16:08:01 +0100

    Reorganized files, removed src folder which is not recommended in Python

--- edge/proto_build/consumer.py
+++ edge/proto_build/consumer.py
@@ -1,8 +1,30 @@
+"""
+SPDX-FileCopyrightText: 2023 Contributors to the Eclipse Foundation
+
+See the NOTICE file(s) distributed with this work for additional
+information regarding copyright ownership.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+"""
 from contextlib import closing
 
 import grpc
 import service_discovery.v1.service_registry_pb2 as service_registry_pb2
 import service_discovery.v1.service_registry_pb2_grpc as service_registry_pb2_grpc
+
+from common import discoverDigitalTwinService
 
 import invehicle_digital_twin.v1.invehicle_digital_twin_pb2 as invehicle_digital_twin_pb2
 import invehicle_digital_twin.v1.invehicle_digital_twin_pb2_grpc as invehicle_digital_twin_pb2_grpc
@@ -19,33 +41,6 @@ INVEHICLE_DIGITAL_TWIN_SERVICE_NAME = "invehicle_digital_twin"
 INVEHICLE_DIGITAL_TWIN_SERVICE_VERSION = "1.0"
 
 mqttClient = None
-
-
-def discoverDigitalTwinService():
-    """
-    Discovers the Digital Twin Service using the CHARIOTT service discovery.
-
-    This function establishes a connection to the CHARIOTT service discovery server
-    and sends a DiscoverRequest to find the Digital Twin Service with the specified
-    namespace, name, and version.
-
-    Returns:
-        The discovered service
-    """
-    print(f"Discovering Digital Twin Service at {CHARIOTT_SERVICE_DISCOVERY_URI} with namespace {INVEHICLE_DIGITAL_TWIN_SERVICE_NAMESPACE} and name {INVEHICLE_DIGITAL_TWIN_SERVICE_NAME}")
-
-
-    with closing(grpc.insecure_channel(CHARIOTT_SERVICE_DISCOVERY_URI)) as channel:
-        stub = service_registry_pb2_grpc.ServiceRegistryStub(channel)
-        request = service_registry_pb2.DiscoverRequest(
-            namespace=INVEHICLE_DIGITAL_TWIN_SERVICE_NAMESPACE,
-            name=INVEHICLE_DIGITAL_TWIN_SERVICE_NAME,
-            version=INVEHICLE_DIGITAL_TWIN_SERVICE_VERSION
-        )
-        response = stub.Discover(request)
-        print(f"Digital Twin Service discovered {response}")
-
-        return response.service
 
 
 def collectRequiredSignalIDs() -> list:
